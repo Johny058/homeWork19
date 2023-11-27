82ec07c9d7ac936c3b4bfc1cbfa093796011692c
Тимур <tuhvatullin_163@mail.ru>
2023-11-28 01:22:14 +0300

    Initial commit

---
+++ src/main/java/pro/sky/homework19/Homework19Application.java
@@ -0,0 +1,13 @@
+package pro.sky.homework19;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework19Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Homework19Application.class, args);
+	}
+
+}
